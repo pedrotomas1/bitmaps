7ad52ed278970df8593c95c764e725535bab83fc
Pedro Tomás dos Santos <pedro.tomas.santos@celfocus.com>
2024-08-14 10:07:35 +0100

    transform bitmaps

---
+++ bitmaps/frame.h
@@ -0,0 +1,10 @@
+frame=[
+  [ 0,0,0,0,0,0,0,0],
+  [ 0,0,0,0,0,0,0,0],
+  [ 0,null,null,null,null,null,null,0],
+  [ 0,null,null,null,null,null,null,0],
+  [ 0,null,null,null,null,null,null,0],
+  [ 0,null,null,null,null,null,null,0],
+  [ 0,null,null,null,null,null,null,0],
+  [ 0,0,0,0,0,0,0,0]
+]
